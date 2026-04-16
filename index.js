d18270a442ce9c6bbea86516d60b9a61ee28b1e1
Devjyoti Mukherjee <devjyotidgr2023@gmail.com>
2026-04-16 13:08:19 +0530

    debugging mongodb

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ connectDB();
 
 const PORT = process.env.PORT || 5000;
 
+console.log("MONGO_URI:", process.env.MONGO_URI);
+
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
